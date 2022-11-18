748261d4a5c93487a9ed219f6ade57b5cb44e3ba
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2022-11-18 10:50:34 -0300

    Update kubernetes_sample_v2.py

--- dags/kubernetes_sample_v2.py
+++ dags/kubernetes_sample_v2.py
@@ -8,7 +8,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
+    'email': [rogeriosilvarocha@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
