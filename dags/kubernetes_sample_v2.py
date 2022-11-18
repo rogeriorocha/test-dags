9151fe53cab9b96cf48259bc403eb727644c572a
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2022-11-18 10:57:38 -0300

    Update kubernetes_sample_v2.py

--- dags/kubernetes_sample_v2.py
+++ dags/kubernetes_sample_v2.py
@@ -24,7 +24,7 @@ start = DummyOperator(task_id='run_this_first', dag=dag)
 passing = KubernetesPodOperator(namespace='ns-airflow-dev',
                           image="python:3.6",
                           cmds=["python","-c"],
-                          arguments=["print('hello world')\nimport time\ntime.sleep(5) "],
+                          arguments=["print('hello world')\nimport time\ntime.sleep(20) "],
                           labels={"foo": "bar"},
                           name="passing-test",
                           task_id="passing-task",
